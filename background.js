d145f94cf0cc64e9f3204958c3f7dc9d397191fd
liuyilun <3090101143@zju.edu.cn>
2015-06-12 09:59:06 +0800

    initial commit

---
+++ background.js
@@ -0,0 +1,71 @@
+var duanList = new Array();
+duanList=[];
+
+var rule = Object();
+rule.top = 2;
+rule.oo = 100;
+rule.ratio = 8.0; 
+
+//var views = chrome.extension.getViews({type: "popup"});
+//for (var i = 0; i < views.length; i++) {
+//    views[i].document.getElementById('x').innerHTML="My Custom Value";
+//}
+
+var restore_options = function () {
+  chrome.storage.sync.get({
+    top:3,
+    oo:100,
+    ratio:10
+  }, function(items) {
+    setRule(items);
+    renderComments();
+  });
+}
+
+function setRule(items){
+    rule.top = items.top;
+    rule.oo = items.oo;
+    rule.ratio = items.ratio;
+}
+
+function renderComments(){
+    $("span[id^='cos_support']").each(function(){
+           var duan = Object();
+           var duanId = this.id.substr(12);
+           var oo = parseInt($(this).text());
+           var xx = parseInt($("span[id^='cos_unsupport-"+duanId+"']").text());
+           if(oo <= 0){
+               oo = 1;
+           }
+           if(xx <= 0){
+               xx = 1;
+           }
+           duan.oo = oo;
+           duan.xx = xx;
+           duan.highlight = false;
+           duan.id = duanId;
+           duanList.push(duan);
+           
+        });
+
+    //$("li[id^='comment']").css({"background":"#ffcfc0"});
+
+    duanList.sort(function(a,b){return a.oo>b.oo?-1:1});
+
+    for (idx in duanList){
+        var aduan = duanList[idx];
+        if(aduan.oo>rule.oo || aduan.oo/aduan.xx > rule.ratio){
+            $('#comment-'+duanList[idx].id).find("p").css({"background":"#ffe9e3"});
+        }
+    }
+
+    for (var i=0 ;i < rule.top; i++){
+        $('#comment-'+duanList[i].id).find("p").css({"background":"#dfffe7"});
+    }
+}
+
+restore_options();
+
+
+
+
---
+++ popup.js
@@ -0,0 +1,50 @@
+//var BGPage = chrome.extension.getBackgroundPage();
+
+$(document).ready(function() {
+    restore_options();
+
+    $("#topvalue").text(topvalue);
+    $("#oovalue").text(oovalue);
+    $("#ratiovalue").text(ratiovalue);
+
+    $("input").on("input", function(){
+        $("#"+this.id+"value").html(this.value);
+    });
+    $("input").on("change",function(){
+        save_options();
+    });
+
+    function save_options() {
+      var top = parseInt($("#topvalue").text());
+      var oo = parseInt($("#oovalue").text());
+      var ratio = parseInt($("#ratiovalue").text());
+      chrome.storage.sync.set({
+        top:top,
+        oo:oo,
+        ratio:ratio
+      }, function() {
+        var status =$("#status");
+        status.html('Options saved.');
+        setTimeout(function() {
+            status.html('&nbsp');
+        }, 3000);
+      });
+    }
+
+    function restore_options() {
+      chrome.storage.sync.get({
+        top:3,
+        oo:100,
+        ratio:10
+      }, function(items) {
+        $("#topvalue").text(items.top);
+        $("#oovalue").text(items.oo);
+        $("#ratiovalue").text(items.ratio);
+        $("#top").val(items.top);
+        $("#oo").val(items.oo);
+        $("#ratio").val(items.ratio);
+      });
+    }
+});
+
+
