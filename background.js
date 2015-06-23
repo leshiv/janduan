106d14ec10f3012bc00d7e72422c9148bee71b44
leshiv <leshiv@hotmail.com>
2015-06-23 15:56:19 +0800

    fix

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ duanList=[];
 var rule = Object();
 rule.top = 2;
 rule.oo = 100;
-rule.ratio = 8.0; 
+rule.ratio = 8.0;
 
 //var views = chrome.extension.getViews({type: "popup"});
 //for (var i = 0; i < views.length; i++) {
@@ -23,49 +23,46 @@ var restore_options = function () {
 }
 
 function setRule(items){
-    rule.top = items.top;
-    rule.oo = items.oo;
-    rule.ratio = items.ratio;
+  rule.top = items.top;
+  rule.oo = items.oo;
+  rule.ratio = items.ratio;
 }
 
 function renderComments(){
-    $("span[id^='cos_support']").each(function(){
-           var duan = Object();
-           var duanId = this.id.substr(12);
-           var oo = parseInt($(this).text());
-           var xx = parseInt($("span[id^='cos_unsupport-"+duanId+"']").text());
-           if(oo <= 0){
-               oo = 1;
-           }
-           if(xx <= 0){
-               xx = 1;
-           }
-           duan.oo = oo;
-           duan.xx = xx;
-           duan.highlight = false;
-           duan.id = duanId;
-           duanList.push(duan);
-           
-        });
-
-    //$("li[id^='comment']").css({"background":"#ffcfc0"});
-
-    duanList.sort(function(a,b){return a.oo>b.oo?-1:1});
-
-    for (idx in duanList){
-        var aduan = duanList[idx];
-        if(aduan.oo>rule.oo || aduan.oo/aduan.xx > rule.ratio){
-            $('#comment-'+duanList[idx].id).find("p").css({"background":"#ffe9e3"});
-        }
+  $("span[id^='cos_support']").each(function(){
+    var duan = Object();
+    var duanId = this.id.substr(12);
+    var oo = parseInt($(this).text());
+    var xx = parseInt($("span[id^='cos_unsupport-"+duanId+"']").text());
+    if(oo <= 0){
+      duan.ratio = 0;
+    }else if(xx <= 0){
+      duan.ratio = oo;
+    }else {
+      duan.ratio = oo/xx;
     }
+    duan.oo = oo;
+    duan.xx = xx;
+    duan.highlight = false;
+    duan.id = duanId;
+    duanList.push(duan);
 
-    for (var i=0 ;i < rule.top; i++){
-        $('#comment-'+duanList[i].id).find("p").css({"background":"#dfffe7"});
+  });
+
+  //$("li[id^='comment']").css({"background":"#ffcfc0"});
+
+  duanList.sort(function(a,b){return a.oo>b.oo?-1:1});
+
+  for (idx in duanList){
+    var aduan = duanList[idx];
+    if(aduan.oo>rule.oo || aduan.ratio > rule.ratio){
+      $('#comment-'+duanList[idx].id).find("p").css({"background":"#ffe9e3"});
     }
+  }
+
+  for (var i=0 ;i < rule.top; i++){
+    $('#comment-'+duanList[i].id).find("p").css({"background":"#dfffe7"});
+  }
 }
 
 restore_options();
-
-
-
-
